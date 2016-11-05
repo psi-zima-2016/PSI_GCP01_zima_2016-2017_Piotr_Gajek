b98b1d639a057d44c5d6898eb622c0e40db49b98
pgajek2 <pgajek2@gmail.com>
2016-11-05 12:53:46 +0100

    Perceptron oraz MCP

--- Neuron.java
+++ Neuron.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -15,27 +16,29 @@ public class Neuron {
 	 	public double[] outputs;
 	 	public int wymiar = 0; 				 
 	 	public int kroki = 50;               //ilosc powtorzen
-	 	public double eta = 0.1;            //współczynik uczenia                 
+	 	public double eta = 0.05;            //współczynik uczenia                 
 	 	public double y = 0;     
 	 	public double theta = 1;//wyjscie neuronu 
 	 	public double localError = 0;
 	 	public Scanner odczyt;
-	 	
+	 	public int in;
 	 	public Neuron()
 	 	{
 	 		try {
 				odczyt = new Scanner(new File("plik.txt"));
+				//odczyt = new Scanner(new File("walidacja.txt"));
 			} catch (FileNotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-	 		wymiar = odczyt.nextInt();;  
-	 		x = new double[2][wymiar];          //20 przykładów
-	 		w = new double[3];      		    //wag tyle co wejść+1   
+	 		in = 2;
+	 		wymiar = odczyt.nextInt();
+	 		x = new double[in][wymiar];          //20 przykładów
+	 		w = new double[in+1];      		    //wag tyle co wejść+1   
 	 	    outputs = new double[wymiar];       //20 wyjść
 	 	    losujWagi();
-	 	   //setWagi(0.592,0.8014, 0.9516);
-	 	   //setWagi(0.8510788611486173 ,0.8679560628223864, 0.991051769906019);
+	 	    //setWagi(0.592,0.8014, 0.9516);
+	 	  //setWagi(0.8225330269184498 ,0.8408765257064726, 0.6887539253404061);
 	 	}		
 
 	 	public void losujWartosci()
@@ -44,12 +47,14 @@ public class Neuron {
 	 		{
 	 			x[0][i] = los.nextInt(20)-10;
 	 			x[1][i] = los.nextInt(20)-10;
+	 			//x[2][i] = los.nextInt(20)-10;
 	 		}
 	 	}
 
+
 	 	public void losujWagi()
 	 	{
-	 		for(int i=0;i<3;i++)
+	 		for(int i=0;i<in+1;i++)
 	 		{
 	 			w[i] = (los.nextDouble() * 2) - 1;
 	 		}
@@ -63,11 +68,11 @@ public class Neuron {
 	 	}
 
 	 	public void ucz() throws FileNotFoundException
-
 	 	{		
+			PrintWriter zapis = new PrintWriter("blad.txt");
 	 				int apt=0;
 					while(odczyt.hasNextDouble())
-						{
+						{	
 	 						x[0][apt]=odczyt.nextDouble();
 	 						x[1][apt]=odczyt.nextDouble();
 	 						outputs[apt]=odczyt.nextDouble();
@@ -75,6 +80,7 @@ public class Neuron {
 						}						
 		 	int iter = 0;	
 		 	double globalError;
+		 	double MSE =0;
 	 		do
 	 		{	
 	 			globalError=0;
@@ -86,27 +92,32 @@ public class Neuron {
 		 			w[0] += eta*localError*x[0][p];
 		 			w[1] += eta*localError*x[1][p];
 		 			w[2] += eta*localError;
-		 			globalError = globalError + (localError*localError);
+		 			globalError = globalError + (localError*localError); //suma bledow
 		 			System.out.println(x[0][p] +" "+x[1][p] +" "+ outputs[p]+" => "+y );
 	 			}
-	 			System.out.println("Iteracja: "+iter+ " MSE = "+Math.sqrt(globalError/wymiar));
+	 			MSE = Math.pow(globalError, 2)/wymiar;
+	 			System.out.println("Iteracja: "+iter+ " MSE = " + MSE );
+		 		zapis.println(MSE);
 	 		} while(globalError!=0 && iter < 10000);
-	 		
-	 		System.out.println("Nauczony! "+ w[0]+" " + w[1] + " " + w[2] );
+	 		zapis.close();
+	 		System.out.println("Nauczony! ");
+	 		System.out.println(w[0]);
+	 		System.out.println(w[1]);
+	 		System.out.println(w[2]);
 	 		//sprawdzamy czy działa
-	 		//losujWartosci();
-	 		for(int i=0;i<wymiar;i++)
-	 		{
-	 			double pom = testuj(x[0][i],x[1][i]);
-		 		System.out.println( x[0][i]+" "+x[1][i]+" => "+ pom);
-	 		}
+//	 		losujWartosci();
+//	 		for(int i=0;i<wymiar;i++)
+//	 		{
+//	 			double pom = testuj(x[0][i],x[1][i]);
+//		 		System.out.println( x[0][i]+" "+x[1][i]+" "+"=> "+ pom);
+//	 		}
 	 		
 		}
 
 		public double licz(double x1, double x2)
 		{
 			suma = 0;
-			suma = x1 * w[0] + x2 * w[1] + w[2]; //sumator
+			suma = x1 * w[0] + x2 * w[1] +w[2]; //sumator
 			
 			//Funkcja progowa unipolarna
     		if(suma >= theta) suma = 1;
@@ -119,7 +130,9 @@ public class Neuron {
 			//Funkcja lniowa
 //			double a = 0.5;
 //			double b = 0;   
-//			suma = a*suma+b;
+//			suma = a*suma+b; Y = AX+B
+//    		if(suma >= 0) suma = 1;
+//			else suma = -1;
 			
 			//Obcięta funkcja linowa
 //			if(suma<-1) suma = -1;
